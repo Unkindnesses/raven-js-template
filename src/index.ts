8ab4800cd81bf04b9acb1a95f6b26adca2f5a936
Mike Innes <mike.j.innes@proton.me>
2026-02-12 20:46:41 +0000

    raven

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+import { log, add, fibSequence } from "./lib.js";
+
+log("hello from js!")
+
+console.log('2 + 3 =', await add(2, 3));
+
+console.log(await fibSequence(10));
